4f9e10335ad33a511cf3dbf51b03cfc65239245e
Tamari Giorgobiani <takogiorgobiani97@gmail.com>
2022-05-10 23:56:37 +0400

    esc key + open and close modal

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-"use strict";
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModel = function () {
+  //   console.log('Button Clicked!');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const clodeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModel);
+
+btnCloseModal.addEventListener('click', clodeModel);
+overlay.addEventListener('click', clodeModel);
+
+// ESC key to close
+
+document.addEventListener('keydown', function (e) {
+  // e stands for event
+  //   console.log(e.key); // we know whick key is pressed
+
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      clodeModel();
+    }
+  }
+});
